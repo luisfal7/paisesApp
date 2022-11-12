8d19c968f17e10181a685e2c7e7f628ab0eac355
Luis Falconi <luism.falconi@gmail.com>
2022-11-12 13:25:27 -0300

    add service paisService

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styleUrls: ['./por-pais.component.css']
+  styleUrls: ['./por-pais.component.css'],
 })
 export class PorPaisComponent {
+  termino: string = '';
 
-  termino: string = ''
-
-  constructor() { }
+  constructor(private PaisService: PaisService) {}
 
   buscar() {
-    console.log(this.termino)
+    console.log(this.termino);
+    //.subcribe() es necesario xq es un observable
+    this.PaisService.buscarPais(this.termino).subscribe((resp) => {
+      console.log(resp);
+    });
   }
-
 }
---
+++ src/app/pais/services/pais.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+
+  private _apiUrl: string = 'https://restcountries.com/v3.1'
+
+  constructor( private http: HttpClient ) { }
+
+  buscarPais( termino: string ): Observable<any> {
+
+    const url = `${ this._apiUrl }/name/${ termino }`
+
+    return this.http.get( url )
+
+  }
+
+
+}
