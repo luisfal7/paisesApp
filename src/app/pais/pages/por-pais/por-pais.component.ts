10b248394cb24803b90f5a381110155f60100e6e
Luis Falconi <luism.falconi@gmail.com>
2022-11-12 21:06:22 -0300

    add error pais

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -8,14 +8,19 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorPaisComponent {
   termino: string = '';
+  hayError: boolean = false
 
   constructor(private PaisService: PaisService) {}
 
   buscar() {
+    this.hayError = false
     console.log(this.termino);
     //.subcribe() es necesario xq es un observable
     this.PaisService.buscarPais(this.termino).subscribe((resp) => {
       console.log(resp);
+    },(err)=>{
+      console.log('Error')
+      this.hayError = true
     });
   }
 }
