081f88daf6547c05048a69080c2c273cb2f93685
Luis Falconi <luism.falconi@gmail.com>
2022-11-13 19:25:43 -0300

    add switchMap RxJs

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -7,18 +8,23 @@ import { PaisService } from '../../services/pais.service';
   templateUrl: './ver-pais.component.html',
 })
 export class VerPaisComponent implements OnInit {
-
-  constructor( private activatedRoute: ActivatedRoute, private PaisService: PaisService ) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private PaisService: PaisService
+  ) {}
 
   ngOnInit(): void {
+    /* this.activatedRoute.params.subscribe(({ id }) => {
+      console.log(id);
+      this.PaisService.getPAisParam(id).subscribe((pais) => {
+        console.log(pais);
+      });
+    }); */
 
-    this.activatedRoute.params.subscribe( ({ id }) => {
-      console.log( id )
-      this.PaisService.getPAisParam(id).subscribe( pais => {
-        console.log(pais)
-      })
-    } )
-
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.PaisService.getPAisParam(id)))
+      .subscribe((resp) => {
+        console.log(resp);
+      });
   }
-
 }
