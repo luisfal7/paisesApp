faff5c60ac72eaad6bd9fa10190fa5a379e36261
Luis Falconi <luism.falconi@gmail.com>
2022-11-14 09:18:06 -0300

    modif pais-input css

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -4,7 +4,6 @@ import { debounceTime, Subject } from 'rxjs';
 @Component({
   selector: 'app-pais-input',
   templateUrl: './pais-input.component.html',
-  styleUrls: ['./pais-input.component.css'],
 })
 export class PaisInputComponent implements OnInit {
 
