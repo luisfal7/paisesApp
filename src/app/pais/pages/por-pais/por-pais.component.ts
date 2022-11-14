c9130ebe11165f00ea0e162365f4c411725de6e2
Luis Falconi <luism.falconi@gmail.com>
2022-11-14 14:45:08 -0300

    add sugerencias buscador

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,31 +5,54 @@ import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
 export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
- 
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false
+
   constructor(private PaisService: PaisService) {}
 
-  buscar( termino: string ) {
-    this.hayError = false
-    this.termino = termino
+  buscar(termino: string) {
+    this.hayError = false;
+    this.termino = termino;
 
     //.subcribe() es necesario xq es un observable
-    this.PaisService.buscarPais(termino).subscribe((paises) => {
-      console.log(paises);
-      this.paises = paises
-    },(err)=>{
-      console.log('Error')
-      this.hayError = true
-      this.paises = []
-    });
+    this.PaisService.buscarPais(termino).subscribe(
+      (paises) => {
+        console.log(paises);
+        this.paises = paises;
+      },
+      (err) => {
+        console.log('Error');
+        this.hayError = true;
+        this.paises = [];
+      }
+    );
   }
 
-  sugerencias( termino: string ) {
-    this.hayError = false
+  sugerencias(termino: string) {
+    this.hayError = false;
+    this.termino = termino
+    this.mostrarSugerencias = true
+
+    this.PaisService.buscarPais(termino).subscribe(
+      (paises) =>this.paisesSugeridos = paises.slice(0, 5),
+      (err) => this.paisesSugeridos = []
+    );
+  }
+
+  buscarSugerido( termino:string ) {
+    this.buscar( termino )
   }
 
 }
